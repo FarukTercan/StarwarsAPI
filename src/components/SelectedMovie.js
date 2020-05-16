48eef62446547ac8211e9fb6caafde3e34daa83a
FarukTercan <trcnfaruk@gmail.com>
2020-05-16 13:20:07 +0200

    SelectedMovie Updated

--- src/components/SelectedMovie.js
+++ src/components/SelectedMovie.js
@@ -1,27 +1,49 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { Card, Container, Button } from 'semantic-ui-react';
 
 function SelectedMovie({ movies }) {
   const { ID } = useParams();
 
   const movieProfile = movies.find((movie) => movie.episode_id == ID);
-  const { title } = movieProfile;
+  const {
+    title,
+    episode_id,
+    opening_crawl,
+    director,
+    release_date
+  } = movieProfile;
 
   return (
     <div className='selected-div'>
-      {movies && (
-        <div className='user-profile'>
-          <h1>Selected Movie</h1>
-          <ul>
-            <li>{title} </li>
-          </ul>
+      <Container>
+        <Card color='red'>
+          <Card.Content>
+            <br />
+
+            <Card.Header>{title}</Card.Header>
+
+            <Card.Description>
+              <strong>Episode {episode_id}</strong>
+              <br />
+              <br />
+              <strong>Director {director}</strong>
+              <br />
+              <br />
+              <strong>{opening_crawl}</strong>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <strong>Release Date {release_date}</strong>
+          </Card.Content>
+        </Card>
+
+        <div className='btn'>
+          <Link to='/'>
+            <Button color='black'>Back to actor search</Button>
+          </Link>
         </div>
-      )}
-      <div className='btn'>
-        <Link to='/'>
-          <button>Back to actor search</button>
-        </Link>
-      </div>
+      </Container>
     </div>
   );
 }
